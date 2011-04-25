87aa1939998a4c3a166d524c3f66df020377cf14
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-25 22:40:57 +0100

    Removed all the demo code, and created a Main file.

--- g52cfj-cw1/src/mainfunction.cpp
+++ g52cfj-cw1/src/mainfunction.cpp
@@ -3,11 +3,7 @@
 #include "templates.h"
 
 // Needs one of the following #includes, to include the class definition
-#include "BouncingBallMain.h"
-#include "MyProjectMain.h"
-#include "Demo2Main.h"
-#include "Demo2aMain.h"
-#include "Demo3Main.h"
+#include "Main.h"
 
 #define BASE_SCREEN_WIDTH 800
 #define BASE_SCREEN_HEIGHT 600
@@ -28,11 +24,7 @@ int main(int argc, char *argv[])
 
 	{
 		// Needs just one of the two following lines:
-		BouncingBallMain oMain;
-		//MyProjectMain oMain;
-		//Demo2Main oMain;
-		//Demo2aMain oMain;
-		//Demo3Main oMain;
+		Main oMain;
 
 		char buf[1024];
 		sprintf( buf, "My Demonstration Program : Size %d x %d", BASE_SCREEN_WIDTH, BASE_SCREEN_HEIGHT );
